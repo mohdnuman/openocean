623bcf1c26cef7c50bd55aa81586c5f77b4dbef3
mohdnuman <mohammadnuman123@gmail.com>
2022-06-01 17:41:26 +0530

    submit ready

--- getData.js
+++ getData.js
@@ -1,7 +1,7 @@
 const Web3 = require("web3");
+const prompt = require("prompt-sync")();
 
-const poolAbi = require("./abi.json");
-const tokenAbi = require("./erc20.json");
+const Abi = require("./abi.json");
 
 let web3;
 
@@ -12,9 +12,9 @@ web3 = new Web3(provider);
 
 async function getBalance1(address) {
   const EthPoolAddress = "0x44Cdb4a71A806c30dfF34a8bDFD59551021e4947";
-  const EthPoolInstance = new web3.eth.Contract(poolAbi, EthPoolAddress);
+  const EthPoolInstance = new web3.eth.Contract(Abi, EthPoolAddress);
   const tokenAddress = "0x7778360F035C589fCE2f4eA5786CbD8B36e5396B";
-  const tokenInstance = new web3.eth.Contract(tokenAbi, tokenAddress);
+  const tokenInstance = new web3.eth.Contract(Abi, tokenAddress);
 
   const LPtokensRecieved = await EthPoolInstance.methods
     .userCollateral(address)
@@ -24,24 +24,29 @@ async function getBalance1(address) {
   let Ethrewards = await EthPoolInstance.methods.pendingReward(address).call();
   Ethrewards = (Ethrewards / 10 ** decimals).toFixed(2);
 
-  const percentage = (LPtokensRecieved / totalSupplyLP) * 100;
+  
+  let stakeToken=await EthPoolInstance.methods.stakeToken().call();
+  let LPtoken=new web3.eth.Contract(Abi,stakeToken);
 
-  const eth = (percentage * 0.377877).toFixed(2);
-  const ooe = (percentage * 12060).toFixed(2);
+  let reserves=await LPtoken.methods.getReserves().call();
+
+  const percentage = (LPtokensRecieved / totalSupplyLP) ;
+
+  const ooe = (percentage * (reserves._reserve0/10**18)).toFixed(2);
+  const eth = (percentage * (reserves._reserve1/10**18)).toFixed(2);
 
   if (eth != 0 && ooe != 0) {
-    console.log("WETH+OOE", eth,"+",ooe);
-    console.log("rewards:", Ethrewards);
+    console.log("OOE+WETH", ooe, "+", eth);
+    console.log("rewards:", Ethrewards,'OOE');
   }
 }
 async function getBalance2(address) {
   const USDCPoolAddress = "0x0d21A1db4d4beFD575E77D5D3F0d0D5f9E54bA44";
-  const UsdcPoolInstance = new web3.eth.Contract(poolAbi, USDCPoolAddress);
+  const UsdcPoolInstance = new web3.eth.Contract(Abi, USDCPoolAddress);
   const tokenAddress = "0x7778360F035C589fCE2f4eA5786CbD8B36e5396B";
-  const tokenInstance = new web3.eth.Contract(tokenAbi, tokenAddress);
+  const tokenInstance = new web3.eth.Contract(Abi, tokenAddress);
 
   const decimals = await tokenInstance.methods.decimals().call();
-
 
   const USDCLPtokensRecieved = await UsdcPoolInstance.methods
     .userCollateral(address)
@@ -55,25 +60,29 @@ async function getBalance2(address) {
     .call();
   USDCrewards = (USDCrewards / 10 ** decimals).toFixed(2);
 
-  const USDCpercentage = (USDCLPtokensRecieved / USDCtotalSupplyLP) * 100;
+  let stakeToken=await UsdcPoolInstance.methods.stakeToken().call();
+  let LPtoken=new web3.eth.Contract(Abi,stakeToken);
 
-  const usdc = (USDCpercentage * 2315.13).toFixed(2);
-  const ooe2 = (USDCpercentage * 21184).toFixed(2);
+  let reserves=await LPtoken.methods.getReserves().call();
+
+  const USDCpercentage = (USDCLPtokensRecieved / USDCtotalSupplyLP);
+
+  const ooe2 = (USDCpercentage * (reserves._reserve0/10**18)).toFixed(2);
+  const usdc = (USDCpercentage * (reserves._reserve1/10**6)).toFixed(2);
 
   if (usdc != 0 && ooe2 != 0) {
-    console.log("USDC+OOE", usdc,"+",ooe2);
-    console.log("rewards:", USDCrewards);
+    console.log("OOE+USDC", ooe2, "+", usdc);
+    console.log("rewards:", USDCrewards ,'OOE');
   }
 }
 
 async function getBalance3(address) {
   const OEEPoolAddress = "0xB99D38eB69214e493B1183FFA3d561FC9F75D519";
-  const OeePoolInstance = new web3.eth.Contract(poolAbi, OEEPoolAddress);
+  const OeePoolInstance = new web3.eth.Contract(Abi, OEEPoolAddress);
   const tokenAddress = "0x7778360F035C589fCE2f4eA5786CbD8B36e5396B";
-  const tokenInstance = new web3.eth.Contract(tokenAbi, tokenAddress);
+  const tokenInstance = new web3.eth.Contract(Abi, tokenAddress);
 
   const decimals = await tokenInstance.methods.decimals().call();
-
 
   let ooe3 = await OeePoolInstance.methods.userCollateral(address).call();
   let OEErewards = await OeePoolInstance.methods.pendingReward(address).call();
@@ -83,11 +92,11 @@ async function getBalance3(address) {
 
   if (ooe3 != 0) {
     console.log("OOE:", ooe3);
-    console.log("rewards:", OEErewards);
+    console.log("rewards:", OEErewards,'OOE');
   }
 }
 
-let address = "0x0ae25fbfc7b88a56123731fe62c9af1ee7507cfc";
-getBalance1(address);
-getBalance2(address);
-getBalance3(address);
+const userAddr = prompt("User Address-");
+getBalance1(userAddr);
+getBalance2(userAddr);
+getBalance3(userAddr);
